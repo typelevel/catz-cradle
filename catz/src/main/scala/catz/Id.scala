2865fd6183a437f68981b1346826cf5eae68f68e
Christopher Davenport <chris@christopherdavenport.tech>
2017-03-16 19:51:28 -0400

    Add Id Examples for catz

---
+++ catz/src/main/scala/catz/Id.scala
@@ -0,0 +1,32 @@
+package catz
+
+
+
+object IdEx1 {
+  import cats.Id
+  import cats.Functor
+
+  val one : Int = 1
+  val onePlusOne: Id[Int] = Functor[Id].map(one)(_ + 1)
+  // onePlusOne: cats.Id[Int] = 2
+}
+
+object IdEx2 {
+  import cats.Id
+  import cats.Monad
+
+  val one : Int = 1
+  val mapOnePlusOne : Id[Int] = Monad[Id].map(one)(_ + 1)
+  // mapOnePlusOne: cats.Id[Int] = 2
+  val flatMapOnePlusOne : Id[Int] = Monad[Id].flatMap(one)(_ + 1)
+  // flatMapOnePlusOne: cats.Id[Int] = 2
+}
+
+object IdEx3 {
+  import cats.Id
+  import cats.Comonad
+
+  val one : Int = 1
+  val coflatMapOnePlusOne : Id[Int] = Comonad[Id].coflatMap(one)(_ + 1)
+  // coflatMapOnePlusOne: cats.Id[Int] = 2
+}
